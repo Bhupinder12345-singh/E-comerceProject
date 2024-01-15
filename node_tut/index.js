9e61ffe96d34b0b789590676bd69596429d77f6e
Bhupinder Singh <gs5193973@gmail.com>
2024-01-15 09:09:57 +0530

    Added

--- node_tut/index.js
+++ node_tut/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-
-app.get("/", (req, resp) => {
-    resp.send("app is working");
-});
-
-app.listen(5000, () => {
+require('./db/config');
+const User =require("./db/User");
+app.post("/register",(req,resp)=>{
+    resp.send("Api is in progress....");
+})
+app.listen(3000, () => {
     console.log("Server is running on port 5000");
 });
